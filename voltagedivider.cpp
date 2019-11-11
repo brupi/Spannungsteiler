a29d466aa4d05b12c34c868b0b65f484068fd722
Jan Brupbacher <jan.brupbacher@hsr.ch>
2019-11-11 10:18:16 +0100

    added scaling in calculation

--- voltagedivider.cpp
+++ voltagedivider.cpp
@@ -41,15 +41,28 @@ double VoltageDivider::getR2()
 
 void VoltageDivider::calculate()
 {
-    double e12[12] = {1.0, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2}; // chooses from array
+    double e12[12] = {1.0, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2}; // E12 values
+    double ratio = u1/u2;
 
+    //normalize ratio
+    int scale = 1;
+    while(ratio >= 10){
+        ratio /= 10.0;
+        scale *= 10;
+    }
+
+    //calc resistors
     for(int i=0 ; i<12 ; ++i){
         for(int j=0 ; j<12 ; ++j){
-            if(abs(((e12[i]+e12[j])/e12[j])-(u1/u2)) < abs(((r1+r2)/r2)-(u1/u2)))
+            if(abs(((e12[i]+e12[j])/e12[j])-(ratio)) <= abs(((r1+r2)/r2)-(ratio)))
             {
                 r1 = e12[i];
                 r2 = e12[j];
             }
         }
     }
+
+    //rescale resistors
+    r1 *= 100 * scale;
+    r2 *= 100;
 }
